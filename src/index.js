c2c420c5a236d45f222625e9e149af76ead8b5c3
Joes-BitGit <josephnalmeida@gmail.com>
2020-10-16 17:55:33 -0700

    Added Time Travel

--- src/index.js
+++ src/index.js
@@ -27,52 +27,23 @@ function Square(props) {
 
 class Board extends Component {
 
-	// components have state by setting this.state in their constructors
-	constructor(props) {
-		// in JS classes, you need to always call super when
-		// defining the constructor of a subclass.
-		// All react component class that have a constructor SHOULD start w/
-		// super(props)
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xNext: true,
-		};
-	}
-
-	handleClick(i) {
-		// make a copy of the array: immutability is important
-		const box = this.state.squares.slice();
-		if (calculateWinner(box) || box[i]) return;
-		box[i] = this.state.xNext ? 'X' : 'O';
-		this.setState({
-			squares: box,
-			xNext: !this.state.xNext,
-		});
-	}
-
 	renderSquare(i) {
 		return (
 			<Square
 				// props that will be sent to the children components
 
 				// when you call setSate, React auto updates the child components
-				value={this.state.squares[i]}
+				value={this.props.squares[i]}
 				// its conventional to use on[Event] names for props 
 				// handle[Event] for the methods which handle the events
-				onClick={() => this.handleClick(i)}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 
 	render() {
-		const winner = calculateWinner(this.state.squares);
-		let status = '';
-		status = !winner ? 'Next player: ' + (this.state.xNext ? 'X' : 'O') : 'Winner: ' + winner;
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -94,15 +65,85 @@ class Board extends Component {
 }
 
 class Game extends Component {
+	// lifting state up again
+	// components have state by setting this.state in their constructors
+	constructor(props) {
+		// in JS classes, you need to always call super when
+		// defining the constructor of a subclass.
+		// All react component class that have a constructor SHOULD start w/
+		// super(props)
+		super(props);
+		this.state = {
+			history: [{
+				squares: Array(9).fill(null),
+			}],
+			xNext: true,
+			stepNumber: 0,
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		// make a copy of the array: immutability is important
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) return;
+		squares[i] = this.state.xNext ? 'X' : 'O';
+		this.setState({
+			// concat over push because of immutability
+			history: history.concat([{
+				squares: squares,
+			}]),
+			stepNumber: history.length,
+			xNext: !this.state.xNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xNext: (step % 2) === 0,
+		});
+	}
+
 	render() {
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ?
+				'Go to move #' + move :
+				'Go to game start';
+			return (
+				// if we were displaying data from a database, 
+				// DB IDs could be used as keys
+				// Keys DO NOT need to be globally unique;
+				// They only need to be unique between components and their siblings
+				<li key={move}>
+					<button
+						onClick={() => this.jumpTo(move)}
+					>
+						{desc}
+					</button>
+				</li>
+			);
+		});
+
+		let status = '';
+		status = !winner ? 'Next player: ' + (this.state.xNext ? 'X' : 'O') : 'Winner: ' + winner;
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div>{status}</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
